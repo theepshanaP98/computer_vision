0dcd784de326603c8ba52381b966713946c3f0ac
theepshanaP98 <theepshanaparamasivam@gmail.com>
2025-08-31 22:54:03 +0530

    Add files via upload

--- Assignment_CV_249336V.py
+++ Assignment_CV_249336V.py
@@ -24,7 +24,7 @@ out, lut = apply_piecewise_lut(img, control_pts)
 
 cv2.imwrite(str(OUTDIR/"q1_output.png"), out)
 plt.figure(); plt.title("Q1 – Transform"); plt.xlabel("Input"); plt.ylabel("Output"); plt.plot(lut); plt.show(); savefig(OUTDIR/"q1_transform.png")
-plt.figure(); plt.subplot(1,2,1); plt.imshow(img, cmap="gray"); plt.title("Original"); plt.axis("off")
+plt.figure(figsize=(10, 5)); plt.subplot(1,2,1); plt.imshow(img, cmap="gray"); plt.title("Original"); plt.axis("off")
 plt.subplot(1,2,2); plt.imshow(out, cmap="gray"); plt.title("Transformed"); plt.axis("off"); plt.show(); savefig(OUTDIR/"q1_compare.png")
 
 #Question 2
@@ -41,8 +41,8 @@ lut_gray = midtone_stretch_lut(70, 150, low_gain=0.2, mid_gain=2.5, high_gain=0.
 white = cv2.LUT(img, lut_white)
 gray = cv2.LUT(img, lut_gray)
 
-plt.figure(); plt.title("Q2 – White Transform"); plt.plot(lut_white); plt.show(); savefig(OUTDIR/"q2_white_transform.png")
-plt.figure(); plt.title("Q2 – Gray Transform"); plt.plot(lut_gray); plt.show(); savefig(OUTDIR/"q2_gray_transform.png")
+plt.figure(figsize=(10, 5)); plt.subplot(1,2,1); plt.title("Q2 – White Transform"); plt.plot(lut_white);
+plt.subplot(1,2,2); plt.title("Q2 – Gray Transform"); plt.plot(lut_gray); plt.show(); savefig(OUTDIR/"q2_transforms_compare.png")
 
 plt.figure(); plt.subplot(1,3,1); plt.imshow(img, cmap="gray"); plt.title("Original"); plt.axis("off")
 plt.subplot(1,3,2); plt.imshow(white, cmap="gray"); plt.title("White Matter"); plt.axis("off")
@@ -59,6 +59,9 @@ lab_corr = cv2.merge([L_corr, a, b])
 img_corr = cv2.cvtColor(lab_corr, cv2.COLOR_LAB2BGR)
 cv2.imwrite(str(OUTDIR/"q3_gamma_corrected.jpg"), img_corr)
 
-plt.figure(); plt.title("Q3 – L Histogram Before"); plt.hist(L.ravel(), bins=256, range=(0,255)); plt.show(); savefig(OUTDIR/"q3_hist_L_before.png")
-plt.figure(); plt.title("Q3 – L Histogram After"); plt.hist(L_corr.ravel(), bins=256, range=(0,255)); plt.show(); savefig(OUTDIR/"q3_hist_L_after.png")
-plt.figure(); plt.title("Q3 – Gamma Corrected Image"); plt.imshow(cv2.cvtColor(img_corr, cv2.COLOR_BGR2RGB)); plt.axis("off"); plt.show()
+plt.figure(figsize=(10, 5)); plt.subplot(1,2,1); plt.title("Q3 – L Histogram Before"); plt.hist(L.ravel(), bins=256, range=(0,255));
+plt.subplot(1,2,2); plt.title("Q3 – L Histogram After"); plt.hist(L_corr.ravel(), bins=256, range=(0,255)); plt.show(); savefig(OUTDIR/"q3_hist_L_compare.png")
+
+# Display the corrected image
+plt.figure(figsize=(10, 5)); plt.subplot(1,2,1); plt.imshow(cv2.cvtColor(ori_img, cv2.COLOR_BGR2RGB)); plt.title("Original"); plt.axis("off")
+plt.subplot(1,2,2); plt.imshow(cv2.cvtColor(img_corr, cv2.COLOR_BGR2RGB)); plt.title("Q3 – Gamma Corrected Image"); plt.axis("off"); plt.show(); savefig(OUTDIR/"q3.png")
